58ac4bc41040f6ae3734b8c7613b97fe1e9d93b5
JoseIvo09 <122129863+JoseIvo09@users.noreply.github.com>
2024-03-07 16:08:37 -0300

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Web IPTV Player - Simplest way to stream m3u8 playlist's online">
+    <meta property="description"
+          content="Web IPTV Player is a Online IPTV Player that allows you to stream TV channels online through the M3U URL. IPTV Web Player also has the facility to play on-demand movies, TV shows, series, and more."/>
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:title"
+          content="Online IPTV Player: How to Watch IPTV on Android, Firestick, PC, and Smart TV using Browser"/>
+    <meta property="og:description"
+          content="Web IPTV Player is a Online IPTV Player that allows you to stream TV channels online through the M3U URL. IPTV Web Player also has the facility to play on-demand movies, TV shows, series, and more."/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="http://web.iptvplayers.com/"/>
+    <meta property="og:site_name" content="Web IPTV Player"/>
+
+
+    <!-- izooto -->
+    <script> window._izq = window._izq || [];
+        window._izq.push(["init"]); </script>
+    <script src="https://cdn.izooto.com/scripts/14caf0982c867223897eb6b292e5690bdc14c84d.js"></script>
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57743546-65"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-57743546-65');
+    </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BVMQGBZX7Y"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-BVMQGBZX7Y');
+    </script>
+
+    <title>Web IPTV Player - Simplest way to stream TV channels online through the M3U URL</title>
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="https://web.iptvplayers.com/img/favicon.png">
+    <!-- Bootstrap core CSS-->
+    <link href="https://web.iptvplayers.com/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="https://web.iptvplayers.com/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="https://web.iptvplayers.com/css/style.css" rel="stylesheet">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="https://web.iptvplayers.com/vendor/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="https://web.iptvplayers.com/vendor/owl-carousel/owl.theme.css">
+</head>
+<body class="login-main-body">
+<div class="topnav">
+    <a href="https://web.iptvplayers.com/index.php#whatis">What is Web IPTV Player</a>
+    <a href="https://web.iptvplayers.com/index.php#howtowatch">How to Watch</a>
+    <a href="https://web.iptvplayers.com/index.php#geo">GEO Unblock</a>
+    <a class="active" href="https://web.iptvplayers.com/index.php#play">Play M3U URL</a>
+</div>
+<section class="login-main-wrapper">
+    <div class="container-fluid pl-0 pr-0">
+        <div class="row no-gutters">
+            <div class="col-md-12 p-5 bg-white full-height">
+                <center>
+                    <div class="text-center" style="max-width:1280px;">
+                        <div class="text-center mb-5 login-main-left-header pt-4">
+                            <img src="https://web.iptvplayers.com/img/favicon.png" class="img-fluid" alt="LOGO">
+
+
+                            <h1 class="mt-3 mb-3">Welcome to Online Web IPTV Player</h1>
+                            <p>Simplest way to stream TV channels online through the M3U URL for free.</p>
+
+                            <p>It also has the facility to play on-demand movies, TV shows, series, and other content.
+                                You can also watch live sporting events, matches, and concerts through the M3U URL. The
+                                IPTV Player allows you to stream the content in HD and full HD streaming quality. You
+                                just need to paste the M3U URL or Playlist to watch them on your PC or smartphone. The
+                                IPTV Player can also stream offline content or videos. You can get the M3U URL from any
+                                IPTV service provider by purchasing the subscription. There are plenty of IPTVs
+                                available for providing the free M3U URL for streaming.</p>
+                            <center>
+                                <script async
+                                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5359332093371752"
+                                        crossorigin="anonymous"></script>
+                                <!-- Resp1 -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-5359332093371752"
+                                     data-ad-slot="1622066377"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </center>
+
+
+                        </div>
+
+                        <form method="post" action="https://web.iptvplayers.com/watch.php" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label id="play">IPTV Playlist URL</label>
+                                <input type="text" name="iptv" id="iptv" class="form-control"
+                                       placeholder="https://xxxxxxx.com/username/password.m3u">
+                            </div>
+                            <strong>OR</strong><br><br>
+                            <div class="form-group">
+                                <label>IPTV File</label>
+                                <input type="file" name="iptvfile" id="iptvfile" class="form-control">
+                            </div>
+                            <div class="mt-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button type="submit" name="submit"
+                                                class="btn btn-outline-primary btn-block btn-lg">Watch
+                                        </button>
+                                        
+                                          <p>Note: Web IPTV Players doesn't contain any playlist, Add your own playlist
+                                            provided by your service provider.</p>
+                                        
+                                        
+                                      
+                                    
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p id="whatis">The Web IPTV Player is an unsophisticated online player to watch live TV
+                                channels and on-demand content through <strong>M3U URL</strong>. Online IPTV Player is
+                                the best IPTV Player to stream the IPTV M3U Playlist since it has no installation. You
+                                can just access the IPTV Player through the Website. This Online IPTV Player is
+                                completely free to stream the content. You can watch as much content in this IPTV Player
+                                without any interruption.</p>
+                                
+                                
+                                                <center>
+                                <script async
+                                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5359332093371752"
+                                        crossorigin="anonymous"></script>
+                                <!-- Resp1 -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-5359332093371752"
+                                     data-ad-slot="1622066377"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                            </center>
+                                        
+
+                            <h2>What does this Online IPTV Player Provide?</h2>
+
+                            <p>This Web IPTV Player offers EPG support, and you can enjoy streaming the content in 4K
+                                quality. You can enable and disable the <strong>Closed Captions</strong> anytime in the
+                                streaming. It also has Dynamic Language Switching for subtitles to watch your favorite
+                                content in your desired language.</p>
+
+                            <p>The IPTV Player doesn’t provide any content for streaming. Streamers have to upload their
+                                content for watching on the Player. The Online Player can change the video streaming
+                                quality to watch the content in the best available quality. The
+                                <strong>full-screen</strong> option at the bottom right corner will stream the content
+                                on the entire screen for the best streaming experience.</p>
+
+                            <p>The IPTV Playlist is <strong>displayed on the left side</strong>, and you can choose the
+                                IPTV content in the Playlist to switch the streaming. There is also a <strong>Search
+                                    function</strong> available at the top to search for the required content in the
+                                Playlist.</p>
+
+                            <p>The <strong>Catch-up </strong>option is available with this Online IPTV Player. Whenever
+                                you are streaming a live TV channel, you can rewatch the live streaming from a specific
+                                time. It also has a <strong>Back to Live</strong> option to get back to live streaming
+                                instantly.</p>
+
+                            <h2 id="howtowatch">How to Watch IPTV on Web IPTV Player</h2>
+
+                            <p>It is quite simple to stream the content on this IPTV Player. You just need to paste the
+                                <strong>IPTV M3U URL</strong> to access the TV channels and on-demand movies, series,
+                                and TV shows.</p>
+
+                            <ul>
+                                <li>Open the <a href="https://web.iptvplayers.com/">Online Web IPTV Player</a> on your
+                                    PC or Smartphone and go to the <strong>Add URL section</strong>.
+                                </li>
+
+                                <li>Provide the <strong>M3U URL</strong> that the IPTV Provider of your choice and
+                                    select Watch
+                                </li>
+
+                                <li>The Online IPTV Player will load the IPTV content. Click <strong>Play</strong> to
+                                    watch the content on the device.
+                                </li>
+                            </ul>
+
+                            <p>You can also stream the <strong>local files</strong> through this Online IPTV Player.
+                                Select<strong> Choose File</strong> option in the Add Playlist section and choose the
+                                <strong>media file </strong>to play on the Player.</p>
+
+                            <p>In addition to this, you can also add a Playlist to the player while streaming the
+                                content. You can select the <strong>Upload New Playlist </strong>option at the top to
+                                add new IPTV Playlists.</p>
+
+                            <h2>How to Get IPTV M3U URL</h2>
+
+                            <p>M3U URL is an IPTV Playlist that contains TV channels and on-demand content. In other
+                                words, it is a collection of live TV channels and on-demand movies, series, and TV
+                                shows. The M3U Playlist can contain as much IPTV content in <strong>HD</strong>,
+                                <strong>4K</strong>, and<strong> 8K</strong> streaming quality.</p>
+
+                            <p>There are several ways available to get the IPTV M3U URL. You can just browse for
+                                <strong>M3U Playlist</strong> and get the URL from a trusted website. Popular IPTV
+                                providers offer M3U URLs with subscriptions. You can visit their official website to
+                                purchase the subscription, and the Providers will offer M3U URLs through the mail.</p>
+
+                            <p><strong>Note:</strong> Check out the <a
+                                        href="https://www.iptvplayers.com/free-iptv-m3u-url-playlists-2024/">IPTV M3U Playlists</a>
+                                for streaming on the Online IPTV Player.</p>
+
+                            <h2 id="geo">Does VPN Necessary for Streaming IPTV?</h2>
+
+                            <p>VPN is a must for streaming content with geo-restrictions.
+                                <strong>Geo-restriction</strong> is an annoying factor while streaming favorite content.
+                                VPN helps in <strong>bypassing the geo-restriction</strong> instantly. You can choose
+                                the server location for the proxy network and stream the content without restriction.
+                                Premium VPNs like <a href="https://www.iptvplayers.com/recommends/nord-vpn/"
+                                                     target="_blank" rel="noreferrer noopener nofollow">NordVPN</a> or
+                                <a href="https://www.iptvplayers.com/recommends/expressvpn/" target="_blank"
+                                   rel="noreferrer noopener nofollow">ExpressVPN</a> provides lots of server around the
+                                world. You can subscribe to these VPNs and protect your streaming with the encrypted
+                                proxy network.</p>
+
+                        </form>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Bootstrap core JavaScript-->
+<script src="https://web.iptvplayers.com/vendor/jquery/jquery.min.js"></script>
+<script src="https://web.iptvplayers.com/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="https://web.iptvplayers.com/vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Owl Carousel -->
+<script src="https://web.iptvplayers.com/vendor/owl-carousel/owl.carousel.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="https://web.iptvplayers.com/js/main.js"></script>
+<footer>
+    <p>Copyright @2024</p>
+</footer>
+
+</body>
+</html>
