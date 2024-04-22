c5b0661bfd4e44fe7e1f4403f48ed13b13f7fd30
JoseIvo09 <122129863+JoseIvo09@users.noreply.github.com>
2024-04-21 21:36:47 -0300

    Add files via upload

---
+++ hls/js/toggle-devtools.js
@@ -0,0 +1,24 @@
+function initializeEruda() {
+    eruda.init();
+    eruda.show(); // Show by default because extension button was just clicked
+    document.getElementById('mobile-devtools-loader').remove(); // Remove loading spinner now that eruda is visible
+}
+
+if (window.eruda) {
+    if (!eruda._isInit) {
+        initializeEruda();
+    } else {
+        const devTools = eruda._$el[0].querySelector('.eruda-dev-tools');
+        if (devTools.offsetHeight > 0)
+            eruda.hide();
+        else
+            eruda.show();
+    }
+} else {
+    let readyStateCheckInterval = setInterval(function() {
+        if (window.eruda) {
+            clearInterval(readyStateCheckInterval);
+            initializeEruda();
+        }
+    }, 40);
+}
