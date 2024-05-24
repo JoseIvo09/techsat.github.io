71f0e7793a61d385fe6e39d6e46ea14df93eddc4
JoseIvo09 <122129863+JoseIvo09@users.noreply.github.com>
2024-05-24 14:13:30 -0300

    Update script2.js

--- Dizer/script2.js
+++ Dizer/script2.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			alert('Por favor, insira ambas as partes editáveis da URL.');
 			return;
 		}
-		var newUrl = "https://m.pgsoft-games.com/" + encodeURIComponent(part1) + "/index.html?btt=1&ot=1744b4161227808290cb752a80b2b299&ops=gnGGdoSD5QQGAdaVdYEHptKRb9nq3ELQidSGBf7uqiHQzYcvcfDmDbVXkR6tjr62kqPk46XxywaG%2Bc%2FwSWJJaVDNJqdCahUG5nXPS%2FOEBvRp7791xXGl9v8zVUb3Y0K8NT9spt50P8hKQ6x203E28YGwRJT0kpKNWmF9yCfbpkGL9OnV2fESx9tHK3pnpHd&l=" + encodeURIComponent(part2) + "&__refer=m.pg-redirect.net&or=static.pgsoft-games.com&__hv=1f8e1d3b";
+		var newUrl = "https://m.pgsoft-games.com/" + encodeURIComponent(part1) + "/index.html?btt=1&ot=1744b4161227808290cb752a80b2b299&ops=gnGGdoSD5QQGAdaVdYEHptKRb9nq3ELQidSGBf7uqiHQzYcvcfDmDbVXkR6tjr62kqPk46XxywaG%2Bc%2FwSWJJaVDNJqdCahUG5nXPS%2FOEBvRp7791xXGl9v8zVUb3Y0K8NT9spt50P8hKQ6x203E28YGwRJT0kpKNWmF9yCfbpkGL9OnV2fESx9tHK3pnpHdD&__refer=m.pg-redirect.net&or=static.pgsoft-games.com&l=" + encodeURIComponent(part2) + "&__hv=1f8e1d3b";
 		myFrame.src = newUrl;
 	}
 
