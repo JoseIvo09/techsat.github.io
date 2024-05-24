9614fe0516dfb5069c72c963a3be9ad579932b25
JoseIvo09 <122129863+JoseIvo09@users.noreply.github.com>
2024-05-23 21:36:44 -0300

    Add files via upload

---
+++ Dizer/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const urlInput = document.getElementById("urlInput");
+  const loadButton = document.getElementById("loadButton");
+  const myFrame = document.getElementById("myFrame");
+
+  loadButton.addEventListener('click', function() {
+    myFrame.src = urlInput.value;
+  });
+
+  function updateClock() {
+    const now = new Date();
+    const time = now.toLocaleTimeString('pt-BR', { 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit' 
+    });
+    document.getElementById("clock").innerHTML = time;
+  }
+
+  setInterval(updateClock, 999);
+});
