96a7a831a5f9a2de46666ad8cc6cca2740bcefe8
JoseIvo09 <122129863+JoseIvo09@users.noreply.github.com>
2024-12-01 17:56:40 -0300

    Update playboy.js

--- Adultos/playboy.js
+++ Adultos/playboy.js
@@ -5,7 +5,7 @@ function play_stream(url) {
 
     if (Hls.isSupported()) {
         var hls = new Hls();
-        hls.loadSource('http://s.apkwuv.xyz:8880/live/demopadexchange/demopad/147354.m3u8');
+        hls.loadSource('https://be2719.rcr22.ams01.cdn255.com/hls2/01/03158/k73f6td892ku_o/index-v1-a1.m3u8?t=muYiR2x0HZGUvDxYaKcqAHLxqlxhyBc6_bYciyuIeQg&s=1733085794&e=10800&f=35410536&srv=03&asn=8167&sp=4000&p=');
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
             video.play();
